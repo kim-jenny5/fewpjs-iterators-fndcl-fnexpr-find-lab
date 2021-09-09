ed32ed9eec01a75127a1764a2bc2336a3fa9bf47
Jenny Kim <kimj1905@gmail.com>
2021-09-09 14:59:50 -0400

    commit tree 6529311401d2ca312acb65af8060dd1098760e15

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// const record = [
+//     { year: "2018", result: "L" },
+//     { year: "2017", result: "W" },
+//     { year: "2016", result: "N/A" }
+// ]
 
+function superbowlWin(arr) {
+    const result = arr.find(win => win.result === "W")
+    // debugger
+    return result ? result.year : undefined
+}
+
+// superbowlWin(record)
